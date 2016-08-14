07d43eaba3a052ec6c51c9b0a3af593d3ff7e55d
kmont12 <kmonteiro1222@gmail.com>
2016-08-13 21:15:27 -0400

    now sends from fields

--- docs/script.js
+++ docs/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
   $("#search").click(function(){
     $("#clickTest").text("click");
-    var path="http://localhost:8888/search";
+    var path="/search/";
 
     var response;
     $.getJSON( path, function(data) {
@@ -21,14 +21,41 @@ $(document).ready(function(){
       });
 
   $("#addRecipe").click(function(){
-    console.log("added");
-    $.post("http://localhost:8888/add/",
-            '{"ID" : 2, "Name" : "Dish"}',
+    var dataPost='{';
+    if (document.getElementById('add_name').value != null && document.getElementById('add_name').value != ""){
+      dataPost=dataPost + '"Name" : "'+ document.getElementById('add_name').value+'",';
+    }
+
+    if (document.getElementById('add_type').value != "null" &&document.getElementById('add_type').value != ""){
+      dataPost=dataPost + '"Type" : "'+ document.getElementById('add_type').value+'",';
+    }
+
+    if (document.getElementById('add_url').value != null && document.getElementById('add_url').value != ""){
+      dataPost=dataPost + '"URL" : "'+ document.getElementById('add_url').value+'",';
+    }
+
+    if (document.getElementById('add_time').value != null && document.getElementById('add_time').value != ""){
+      dataPost=dataPost + '"cooktime" : "'+ document.getElementById('add_time').value+'",';
+    }
+
+    if (document.getElementById('add_key').value != null && document.getElementById('add_key').value != ""){
+      dataPost=dataPost + '"keywords" : "'+ document.getElementById('add_key').value+'",';
+    }
+
+    if (document.getElementById('add_rate').value != null && document.getElementById('add_rate').value != ""){
+      dataPost=dataPost + '"rating" : "'+ document.getElementById('add_rate').value+'",';
+    }
+
+    console.log(dataPost);
+    dataPost=dataPost.slice(0,-1);
+    dataPost=dataPost+'}';
+    $.post("/add/",
+            dataPost,
             function(data, status) {
               $("#clickTest").text(status);
             }
           );
-    console.log("after");
+    console.log(dataPost);
   });
 
 <!--$("#siteloader").html('<object data="http://allrecipes.com/recipe/234592/buffalo-chicken-stuffed-shells/"/>');-->
